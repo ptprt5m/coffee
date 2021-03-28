3c0912573b5bf5bc49188b340745bfb33f0f4252
ptprt5m <67976625+ptprt5m@users.noreply.github.com>
2021-03-28 12:19:45 +0300

    iPad breackpoint

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ window.addEventListener("load", function () {
     let form = document.querySelector('.modalWindow__form')
     let inputs = form.querySelectorAll('.modalWindow__form-text')
     let menu = document.querySelector('.header__links')
+    let openModal = document.querySelector('[href="#openModal"]')
+    let close = document.querySelector('[href="#close"]')
+    let closeBurger = document.querySelector('[href="#closeBurger"]')
+    let burger = document.querySelector('.header__burger')
+
+    // Прокрутка по ссылкам меню
 
     menu.addEventListener('click', function(e){ 			
 		let link = e.target 								
@@ -16,7 +22,7 @@ window.addEventListener("load", function () {
     function scrollToTarget(id){							
 		let target = document.querySelector(id)				
 		if(target !== null){								
-			let pos = target.offsetTop
+			let pos = target.offsetTop - 100
 			window.scrollTo({
 				top: pos,
 				behavior: 'smooth'
@@ -24,6 +30,19 @@ window.addEventListener("load", function () {
 		}
 	}
 
+    //Открытие модалки
+
+    openModal.addEventListener('click', function () {
+        document.body.style.overflow = 'hidden'
+        modalWindow.classList.add("modalWindowVisible")
+    })
+    close.addEventListener('click', function () {
+        document.body.style.overflow = 'visible'
+        modalWindow.classList.remove("modalWindowVisible")
+    })
+
+    //Простая проверка на заполнение полей формы модалки
+    
     form.addEventListener("submit", validations) 
     function validations(e) {
         for(let i = 0; i < inputs.length; i++) {
@@ -39,12 +58,16 @@ window.addEventListener("load", function () {
         }
     })
 
-    document.querySelector('[href="#openModal"]').addEventListener('click', function () {
-      document.body.style.overflow = 'hidden'
-      modalWindow.classList.add("modalWindowVisible")
+    burger.addEventListener('click', function(e){
+        document.querySelector('#header__links').classList.remove('header__links')
+        document.querySelector('#header__links').classList.add('burger__box')
+        document.querySelector('#header__links').style.display = block
+        document.querySelector('.header__link-toOrder').style.display = block
     })
-    document.querySelector('[href="#close"]').addEventListener('click', function () {
-      document.body.style.overflow = 'visible'
-      modalWindow.classList.remove("modalWindowVisible")
+    closeBurger.addEventListener('click', function(){
+        document.querySelector('#header__links').classList.add('header__links')
+        document.querySelector('#header__links').classList.remove('burger__box')
+        document.querySelector('#header__links').style.display = none
     })
+
 })
